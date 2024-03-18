8d4dee43a9f03a5d0f71cc0eaeaacd1a680db0fd
happy394 <happy25184@gmail.com>
2024-03-18 12:30:45 +0300

    own vector beginning (needs to be fixed)

--- vector/main2.cpp
+++ vector/main2.cpp
@@ -5,5 +5,6 @@ int main(){
     nonstd::vector<int> v;
     for (int i: {1, 2, 3, 4, 5})
         v.push_back(i);
+    v[2] = 10;
     std::cout << v;
 }
--- vector/vector.h
+++ vector/vector.h
@@ -12,15 +12,78 @@ namespace nonstd
         vector(); // constructor
         ~vector(); // destructor (deletes)
         void push_back(T);
-        size_t size();
+        size_t size() const;
+        T& operator[](size_t);
         T operator[](size_t) const;
-        friend std::ostream& operator<<(std::ostream&, const vector<T>&);
+//        friend std::ostream& operator<<(std::ostream&, const vector<T>&);
 
     private:
-        T* _begin, _end;
+        T* _begin;
+        T* _end;
         size_t _MAX_SIZE;
     };
 
+    template<typename T>
+    vector<T>::vector() {
+        _begin = new T[INIT_SIZE];
+        _MAX_SIZE = INIT_SIZE;
+        _end = _begin;
+    }
+
+    template<typename T>
+    vector<T>::~vector() {
+        delete[] _begin; // deletes container
+    }
+
+    template<typename T>
+    void vector<T>::push_back(T elem){
+        if (_end - _begin >= _MAX_SIZE - 1){
+            _MAX_SIZE *= 2;
+            T* tempPtr = new T[_MAX_SIZE];
+            for (size_t i = 0; i < _end - _begin; i++){
+                tempPtr[i] = _begin[i];
+            }
+            delete[] _begin;
+            _begin = tempPtr;
+            push_back(elem);
+        }
+        else{
+            *_end = elem;
+            _end++;
+        }
+    }
+
+    template<typename T>
+    size_t vector<T>::size() const {
+        return _end - _begin;
+    }
+
+    template<typename T>
+    T& vector<T>::operator[](size_t iter) {
+        if (iter >= size())
+            throw std::out_of_range("index is invalid.");
+        return _begin[iter];
+    }
+
+    template<typename T>
+    T vector<T>::operator[](size_t iter)const {
+        if (iter >= size())
+            throw std::out_of_range("index is invalid.");
+        return _begin[iter];
+    }
+
+    template<typename T>
+    // friend function
+    std::ostream& operator<<(std::ostream& os, const vector<T>& vector){
+//        for (T* i = vector._begin; i < vector._end; i++){
+//            os << *i << ' ';
+//        }
+//        os << '\n';
+        for (size_t i = 0; i < vector.size(); i++){
+            os << vector[i] << ' ';
+        }
+        return os;
+    }
 }
 
 
